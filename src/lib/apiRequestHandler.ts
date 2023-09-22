e4608adc153bfa59327d9f13c250b34781dcabe3
tovehydmark <tovehydmark@gmail.com>
2023-09-22 10:10:21 +0200

    function to handle api requests

---
+++ src/lib/apiRequestHandler.ts
@@ -0,0 +1,21 @@
+export default async function apiRequestHandler(graphqlQuery: string) {
+  try {
+    const response = await fetch('http://localhost:5000/graphql', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ query: graphqlQuery }),
+    });
+
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+
+    const data = await response.json();
+    return data.data;
+  } catch (error) {
+    console.error('Error:', error);
+    throw error;
+  }
+}
