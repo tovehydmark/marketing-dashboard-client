594c9034b4284d06fa3c1001d6ca9f397a0f3d18
tovehydmark <tovehydmark@gmail.com>
2023-09-24 11:55:32 +0200

    print channels data and stats works

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,43 @@
 import './App.css';
 import apiRequestHandler from '../src/lib/apiRequestHandler';
+import { useState } from 'react';
 
 function App() {
-  const getChannels = async () => {
+  const [channelData, setChannelData] = useState();
+
+  const displayChannelsData = async () => {
     const graphqlQuery = `{
           channel
         }`;
 
-    const data = await apiRequestHandler(graphqlQuery);
-    console.log('data', data);
+    const retrieveChannels = await apiRequestHandler(graphqlQuery);
+
+    //Returns unique values from given array
+    function getUniqueValues(value: string, index: number, array: string[]) {
+      return array.indexOf(value) === index;
+    }
+
+    //Returns all possible values within the channels array
+    const possibleValues = retrieveChannels.channel.filter(getUniqueValues);
+
+    //Map through the possible values and count how many times they occur within the original array
+    const printChannelsData = possibleValues.map((currentChannel: string, i: number) => {
+      let counter = 0;
+      for (const channelToFilterOf of retrieveChannels.channel) {
+        if (channelToFilterOf === currentChannel) {
+          counter++;
+        }
+      }
+
+      return (
+        <tr key={i}>
+          <td>{currentChannel}</td>
+          <td>{counter}</td>
+        </tr>
+      );
+    });
+
+    setChannelData(printChannelsData);
   };
   const getChannegroups = async () => {
     const graphqlQuery = `{
@@ -29,9 +58,19 @@ function App() {
 
   return (
     <div className="App">
-      <button onClick={getChannels}>Get channels</button>
+      <button onClick={displayChannelsData}>Get channels</button>
       <button onClick={getChannegroups}>Get channel groups</button>
       <button onClick={getCampaigns}>Get campaigns</button>
+
+      <table>
+        <thead>
+          <tr>
+            <th>Channel</th>
+            <th>Number of orders</th>
+          </tr>
+        </thead>
+        <tbody>{channelData}</tbody>
+      </table>
     </div>
   );
 }
