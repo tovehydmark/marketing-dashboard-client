e07008a85771ee6ab4065cb43c3931517dd2a238
tovehydmark <tovehydmark@gmail.com>
2023-09-24 14:53:17 +0200

    function to get unique values from array

---
+++ src/components/GetUniqueValues.ts
@@ -0,0 +1,4 @@
+export default function GetUniqueValues(value: string, index: number, array: string[]) {
+  //Returns unique values contained in a given array
+  return array.indexOf(value) === index;
+}
